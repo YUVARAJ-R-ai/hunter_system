3dc1a06651cac94acbfd948c8363da952e722419
YUVARAJ-R-ai <yuvaraj28022005@gmail.com>
2026-06-08 15:59:45 +0530

    add new table schema and build the entire apk

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.hunter.system.hunter_system_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21 // explicitly set minSdk to support desugaring properly if needed (or keep flutter.minSdkVersion if it is >= 21)
+        minSdk = flutter.minSdkVersion // explicitly set minSdk to support desugaring properly if needed (or keep flutter.minSdkVersion if it is >= 21)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -45,5 +45,5 @@ flutter {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
